ccbd8a31b2b9a431871794935e9bf1c4bd9dee82
Nick <djhitekdieant@gmail.com>
2021-07-15 11:28:32 +0300

    added some cringe

--- Senbonzakura.Services/GoodsDBRepository.cs
+++ Senbonzakura.Services/GoodsDBRepository.cs
@@ -17,8 +17,8 @@ namespace Senbonzakura.Services
             allGoods = new List<Good>()
             {
                 new Good() {Color = "black", Id = 1, PhotoPath = "smth.png", Type = GoodsKinds.skirt},
-                new Good() {Color = "white", Id = 2, PhotoPath = "smth2.png", Type = GoodsKinds.pant},
-                new Good() {Color = "blue", Id = 3, PhotoPath = "smth3.png", Type = GoodsKinds.sundress},
+                new Good() {Color = "white", Id = 2, PhotoPath = "smth2.jpg", Type = GoodsKinds.pant},
+                new Good() {Color = "blue", Id = 3, Type = GoodsKinds.sundress},
                 new Good() {Color = "green", Id = 4, Type = GoodsKinds.pant}
             };
         }
