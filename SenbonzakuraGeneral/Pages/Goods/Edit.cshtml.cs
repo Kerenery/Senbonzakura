ae0013a2dd732aabf7937d9d627cf01dbad5f001
Nick <djhitekdieant@gmail.com>
2021-07-19 21:13:25 +0300

    Edit page

---
+++ SenbonzakuraGeneral/Pages/Goods/Edit.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Senbonzakura.Models;
+using Senbonzakura.Services;
+
+namespace SenbonzakuraGeneral.Pages.Goods
+{
+    public class EditModel : PageModel
+    {
+
+        private readonly IGoodRepository _database;
+
+        public EditModel(IGoodRepository database)
+        {
+            _database = database;
+        }
+
+        public Good Good { get; set; }
+        public IActionResult OnGet(int id)
+        {
+            Good = _database.GetGood(id);
+
+            if(Good == null) return RedirectToPage("/NotFound");
+
+            return Page();
+        }
+    }
+}
